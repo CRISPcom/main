2666d671285ce28a04ab642ecafe3278beda7e48
uazech <43653336+uazech@users.noreply.github.com>
2020-01-24 09:04:39 +0100

    Update commandes.sh

--- Postgresql/commandes.sh
+++ Postgresql/commandes.sh
@@ -5,3 +5,18 @@ cd PostGresql/insert/
 
 # On insert les tweets
 python insertSomeTweets.py
+
+# configure docker for remote connexions 
+sudo systemctl edit docker.service
+
+# put in nano
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd -H fd:// -H tcp://127.0.0.1:2375
+
+# save the file (ctrl+x)
+
+sudo systemctl daemon-reload
+sudo systemctl restart docker.service
+# check for 0.0.0.0
+sudo netstat -lntp | grep dockerd
